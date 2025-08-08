32c3b60f8d4af2937704a0d62bb30772166f7072
吉田竜弥 <ryuya.yoshida@rakus-partners.co.jp>
2025-08-08 17:22:32 +0900

    20250808

--- src/node/index.js
+++ src/node/index.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//クロスオリジンの設定
+const cors = require('cors');
+app.use(cors({
+  // 許可するオリジンを指定
+  origin: 'http://localhost',
+  // Cookie 等の認証情報を許可 //sessionでデータの受け渡しに必要
+  credentials: true,            
+}));
+
+//セッションの設定
+const session = require("express-session");
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 
 const port = 5062;
 
-const cors = require("cors");
-app.use(cors());
 
 const { Pool } = require("pg");
 const pool = new Pool({
-  // user: "user_5062",
-  // host: "db",
-  // database: "crm_5062",
-  // password: "pass_5062",
   user: process.env.POSTGRES_USER, // PostgreSQLのユーザー名に置き換えてください
   host: "db",
   database: process.env.POSTGRES_DB, // PostgreSQLのデータベース名に置き換えてください
@@ -34,9 +48,31 @@ app.get("/customers", async (req, res) => {
   }
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
+
+
+//確認画面へのデータ受け渡し用エンドポイント
+app.post('/add-confirm', (req, res) => {
+  req.session.customer = req.body;   // データをセッションに保存
+  // req.session.save(err => {
+  //   if (err) {
+  //     return res.status(500).json({ error: 'Session save failed' });
+  //   }
+  // });
+  console.log(req.session.customer);
+  res.json({ success: true });
+  // res.redirect('/add-confirm'); // 確認画面へリダイレクト
+});
+
+//確認画面へセッション情報を渡す
+app.get('/add-confirm', (req, res) => {
+  // res.json({ customer: req.session });
+  console.log(req.session.customer);
+  const sessionCustomer = req.session.customer || {};
+  res.json({ customer: sessionCustomer });
+});
+
+//顧客情報をDBに登録するエンドポイント
 app.post("/add-customer", async (req, res) => {
   try {
     const { companyName, industry, contact, location } = req.body;
